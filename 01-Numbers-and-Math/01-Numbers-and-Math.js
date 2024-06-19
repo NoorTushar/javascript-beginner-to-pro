32f2b10e3372aa7ee5fdf114dd91b1e01aaebecb
NoorTushar <tushar.noor.khan@gmail.com>
2024-06-20 02:19:25 +0600

    first commit

---
+++ 01-Numbers-and-Math/01-Numbers-and-Math.js
@@ -0,0 +1,20 @@
+// Todo: let's say we have a cart which has two items of $10.9 and one $20.95
+// simple calculation
+
+console.log(10.9 * 2 + 20.95); // = $42.75
+
+// NOTE: Order of operation (Order of precedence)
+// Multiplication/ Division is done first and Addition/ Subtraction is done later
+
+/*
+NOTE: Order of operation (Order of precedence)
+Multiplication/ Division is done first and Addition/ Subtraction is done later.
+
+If we get multiplication and division only, the order of the operation matters.
+If its (4*5/10) = 4 * 5 first and then divided by 10
+
+Same with addition/ subtraction.
+*/
+
+// Todo: add shipping fee $4.99
+console.log(10.9 * 2 + 20.95 + 4.99); // =$47.74
